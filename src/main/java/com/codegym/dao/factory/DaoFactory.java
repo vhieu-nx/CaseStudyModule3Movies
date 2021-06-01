4772e7f8c3fcfd5035712197ecd7d30ed1289bc3
vhieu-nx <hieunvbhaf180070@fpt.edu.vn>
2021-06-01 21:43:43 +0700

    fix dao factory

--- src/main/java/com/codegym/dao/factory/DaoFactory.java
+++ src/main/java/com/codegym/dao/factory/DaoFactory.java
@@ -1,6 +1,24 @@
 package com.codegym.dao.factory;
 
+import com.codegym.dao.MovieDao;
+import com.codegym.dao.impl.MovieDAOImpl;
+
 public class DaoFactory {
     private static volatile DaoFactory instance;
-    private final Moi
+    private final MovieDao movieDao = new MovieDAOImpl();
+    private DaoFactory(){}
+    public static DaoFactory getInstance() {
+        if (instance == null) {
+            synchronized (DaoFactory.class) {
+                if (instance == null) {
+                    instance = new DaoFactory();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public MovieDao getMovieDao() {
+        return movieDao;
+    }
 }
